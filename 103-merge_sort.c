9f559f4187ac580717bb704ccf77479dabbb44ea
gichobih <dennismuri254@gmail.com>
2024-05-01 04:54:51 -0700

    103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -13,57 +13,57 @@
  **/
 void merge(int *array, size_t size, int *left, size_t size_left, int *right, size_t size_right)
 {
-	size_t i = 0, j = 0, k = 0;
-	int *temp = malloc(sizeof(int) * size);
+    size_t i = 0, j = 0, k = 0;
+    int *temp = malloc(sizeof(int) * size);
 
-	if (temp == NULL)
-	{
-		free(temp);
-		return;
-	}
+    if (temp == NULL)
+    {
+        free(temp);
+        return;
+    }
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(left, size_left);
-	printf("[right]: ");
-	print_array(right, size_right);
+    printf("Merging...\n");
+    printf("[left]: ");
+    print_array(left, size_left);
+    printf("[right]: ");
+    print_array(right, size_right);
 
-	while (i < size_left && j < size_right)
-	{
-		if (left[i] <= right[j])
-		{
-			temp[k] = left[i];
-			i++;
-		}
-		else
-		{
-			temp[k] = right[j];
-			j++;
-		}
-		k++;
-	}
+    while (i < size_left && j < size_right)
+    {
+        if (left[i] <= right[j])
+        {
+            temp[k] = left[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = right[j];
+            j++;
+        }
+        k++;
+    }
 
-	while (i < size_left)
-	{
-		temp[k] = left[i];
-		i++;
-		k++;
-	}
+    while (i < size_left)
+    {
+        temp[k] = left[i];
+        i++;
+        k++;
+    }
 
-	while (j < size_right)
-	{
-		temp[k] = right[j];
-		j++;
-		k++;
-	}
+    while (j < size_right)
+    {
+        temp[k] = right[j];
+        j++;
+        k++;
+    }
 
-	for (i = 0; i < size; i++)
-		array[i] = temp[i];
+    for (i = 0; i < size; i++)
+        array[i] = temp[i];
 
-	printf("[Done]: ");
-	print_array(array, size);
+    printf("[Done]: ");
+    print_array(array, size);
 
-	free(temp);
+    free(temp);
 }
 
 /**
@@ -74,21 +74,21 @@ void merge(int *array, size_t size, int *left, size_t size_left, int *right, siz
  **/
 void merge_sort(int *array, size_t size)
 {
-	size_t mid = 0, i = 0;
-	int *left = NULL;
-	int *right = NULL;
+    size_t mid = 0, i = 0;
+    int *left = NULL;
+    int *right = NULL;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	mid = size / 2;
+    mid = size / 2;
 
-	left = array;
-	right = array + mid;
+    left = array;
+    right = array + mid;
 
-	merge_sort(left, mid);
-	merge_sort(right, size - mid);
+    merge_sort(left, mid);
+    merge_sort(right, size - mid);
 
-	merge(array, size, left, mid, right, size - mid);
+    merge(array, size, left, mid, right, size - mid);
 }
 
